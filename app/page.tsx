f24f15dccb6e3b24674b775e8c3800888a378d5d
masaki-playground-repo <masaki.yamada@zohomail.jp>
2026-07-12 13:40:10 +0900

    fix: ひと段落したリファクタリングをマージ (#52)
    * fix: サーバー側fetchへの移行のため、リファクタリング (#48)
    
    * 実験コメントを追加
    
    * なんか動いた
    
    * fix: API取得処理サーバー分離に伴いリファクタリング。ねこ画像取得失敗を修正
    
    * fix: MainContent.tsx側のコメントを再調整。
    
    * fix: API取得処理サーバー分離に伴いリファクタリング (#51)
    
    * 実験コメントを追加
    
    * なんか動いた
    
    * fix: API取得処理サーバー分離に伴いリファクタリング。ねこ画像取得失敗を修正
    
    * fix: MainContent.tsx側のコメントを再調整。
    
    * fix: useEffect削除、InitGitItemsの型定義、未使用setRepoの削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,256 +1,38 @@
-// クライアント側で実行
-"use client";
+// サーバー側で実行
 
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+// MainContent.tsxにデータを受け渡すためにインポート
+import MainContent from "@/components/MainContent";
 
-// 状態管理のためのuseState,useEffectを使用
-import { useState, useEffect } from "react";
-
-// ねこ画像をより効率的に扱うためにnext / imageコンポーネントを使用(ESLintの指摘に対応)
-import Image from "next/image";
-
-// GitHub APIからの受信データ（オブジェクト）を定義するインターフェース
-interface GitHubRepo {
-  id: number;
-  name: string;
-  html_url: string;
-  description: string | null; // 説明文は空(null)の場合もある
-  stargazers_count: number;
-  language: string | null; // プロフィールやMarkdownのみのリポジトリは言語未設定(null)
+// GitHubリポジトリ情報の取得関数
+async function getGitData() {
+  const gitres = await fetch("https://api.github.com/users/masaki-y-devops/repos?per_page=8&sort=updated");
+  if (!gitres.ok) throw new Error("failed")
+  return gitres.json();
 }
 
-export default function Home() {
+// ねこ画像の取得関数
+async function getCatData() {
+  const catres = await fetch("https://api.thecatapi.com/v1/images/search")
+  if (!catres.ok) throw new Error("failed")
+  const catdata = await catres.json();
+  return catdata === "" ? null : catdata[0].url;
+}
 
-  // 変数を操作するための「関数」宣言
-  // 上からGitHub情報、ねこ画像URLを格納する。
-  const [repos, setRepos] = useState<GitHubRepo[] | null>([]);
-  const [catImageUrl, setCatImageUrl] = useState<string | null>(null);
+// メインで実行される関数
+// Promise.allで上記API取得関数を並列実行
+export default async function Page() {
+  const [gitData, catData] = await Promise.all([
+    getGitData(),
+    getCatData()
+  ]);
 
-  // useEffectによる関数のトリガー
-  useEffect(() => {
-    // APIの応答がある前に画面遷移され、State更新が空振りしないようにするフラグ(ESLintエラー対策)
-    let isMounted = true;
-
-    async function fetchAllData() {
-      try {
-        // 以下の三項演算子による未取得状態の判定用
-        setRepos(null);
-        setCatImageUrl(null);
-
-        // 複数のfetchをPromise.allで同時に行う（表示速度向上のため）
-        const [gitres, catres] = await Promise.all([
-          // 全取得だとレートリミットに達しやすいため、per_page=を追加して取得件数自体を絞ってみる
-          fetch("https://api.github.com/users/masaki-y-devops/repos?per_page=8&sort=updated"),
-          fetch("https://api.thecatapi.com/v1/images/search")
-        ]);
-
-        const data = await gitres.json();
-        const images = await catres.json();
-
-        // isMountedフラグを見て画面上にあるときだけ取得値をセット
-        if (isMounted){
-
-          // githubより取得したデータが正常取得（配列）の場合のみ当該データをセット（これがないとサイト全体が落ちてしまった）
-          if (Array.isArray(data)) {
-            setRepos(data);   // sliceしない。fetch段階で絞っているため。
-          }
-          
-          setCatImageUrl(images[0].url);
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-    fetchAllData();
-    
-    return () => {
-      // 画面から消えるときに実行されるクリーンアップ関数
-      // APIの応答がある前に画面遷移がされた場合、フラグを折る→State更新がされずエラー防止
-      isMounted = false;
-    }
-  }, []);
-
-  
-  // 猫画像のonLoad時に作動する関数
-  const whenImageLoaded = () => {
-    // ボタンが押下されたことを示すフラグの取得を試行して代入
-    const shouldScroll = localStorage.getItem('shouldScrollToCat');
-
-    // もし取得が成功すれば、id=cat_sectionまで移動
-    if (shouldScroll === 'true'){
-      // 猫画像のセクションの位置を特定
-      const catElement = document.getElementById('cat_section');
-
-      // 自動スクロール
-      if (catElement) {
-        catElement.scrollIntoView({ behavior: 'auto', block: 'start' })
-      }
-
-      // 後始末としてフラグをクリア
-      localStorage.removeItem('shouldScrollToCat');
-    }
-  }
-  
-  // 問い合わせボタン用処理
-  const SpawnBtnClick = () => {
-	
-    // ボタンが押されたことを示すフラグデータを保存しておく
-    localStorage.setItem('shouldScrollToCat', 'true');
-
-    // ボタン押下時のねこ画像更新用
-    async function SpawnNewCat() {
-      try {
-        setCatImageUrl(null);
-        const catres = await fetch("https://api.thecatapi.com/v1/images/search");
-        const images = await catres.json();
-        console.log("SpawnNewCat: 新しいねこを呼びました", images);
-        setCatImageUrl(images[0].url);
-      } catch (error) {
-        console.error('通信エラー', error)
-      }  
-    }
-
-    SpawnNewCat();
-  }
-
-  // スキルデータの配列
-  const skills = [
-    { name: "Shell", level: "中級？" },
-    { name: "Nix", level: "初級" },
-    { name: "C#", level: "初級" },
-    { name: "TypeScript", level: "初級" },
-    { name: "Python", level: "初級" },
-    { name: "Haskell", level: "初級" },
-  ];
-
-  // return以下の要素が動的に変化する
+  // MainContent.tsxに処理結果を投げる
   return (
-    <div className="flex flex-col h-screen overflow-hidden">
-      <Header />
-      <main className="h-screen bg-slate-50 p-8 text-slate-900 overflow-y-auto">
-        <div className="max-w-3xl mx-auto w-full">
-          <section className="mt-12">
-            <h2 className="text-center text-xl font-bold mb-6 border-b-2 border-indigo-200 pb-2">
-              勉強中の分野（言語）
-            </h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-              
-              {/* mapを使ってループ処理 */}
-              {skills.map((skill) => (
-                <div key={skill.name} onClick={() => alert(`${skill.name}を学習中。現在、${skill.level}です。`)} 
-                className="bg-white p-4 rounded-lg shadow-sm border border-slate-200 flex items-center justify-between cursor-pointer hover:shadow-md transition-shadow">
-                  <div>
-                    <h3 className="font-bold">{skill.name}</h3>
-                  </div>
-
-                  {/* 条件分岐 レベルによって背景色を変えてみる */}
-                  <span className={`px-3 py-1 rounded-full text-white text-xs font-bold ${
-                    skill.level === "上級（自称）" ? "bg-red-500" : 
-                    skill.level === "中級？" ? "bg-green-500" : 
-                    skill.level === "初級" ? "bg-blue-500" :
-                    "bg-slate-400"
-                  }`}>
-                    {skill.level}
-                  </span>
-                </div>
-              ))}
-            </div>
-          </section>
-      
-          <section className="mt-12">
-            <h2 className="text-center text-xl font-bold mb-6 border-b-2 border-indigo-200 pb-2">
-              公開中のGitHubリポジトリ
-            </h2>
-
-            {/* reposの中身で判定する三項演算子。API情報取得前はnullであるため後者の処理となり、データが入り次第前者の表示がされる */}
-            {repos ? (
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              {repos.map((repo: GitHubRepo) => (
-                <a 
-                  key={repo.id} 
-                  href={repo.html_url} 
-                  target="_blank" 
-                  className="block bg-white p-4 rounded-lg shadow-sm border border-slate-200 hover:border-indigo-400 transition-colors"
-                >
-                  <h3 className="font-bold text-indigo-600">{repo.name}</h3>
-                  <p className="text-sm text-slate-500">{repo.description || "No description"}</p>
-                  <div className="mt-2 text-xs text-slate-400">Language: {repo.language || "N/A"}</div>
-                </a>
-              ))}
-              </div>
-            ) : (
-              <div className="flex justify-center">
-                <p className="text-center py-8 text-gray-500 text-sm animate-pulse">
-                  リポジトリ情報を読み込み中...<br />
-                  すぐに完了しない場合GitHub側の読み込み頻度制限を受けている場合があります。<br />
-                  時間をおいて再度お試しください。
-                </p>
-              </div>
-            )}
-          </section>
-
-          <section className="mt-12">
-            <h2 className="text-center text-xl font-bold mb-6 border-b-2 border-indigo-200 pb-2">
-              お気に入りの曲
-            </h2>
-            {/* Spotify公開プレイリストとの連携 */}
-            {/* 自分のSpotifyアカウントに公開プレイリストを作成して紐付け、アカウント側の操作（曲の追加・削除）により動的に変化させる */}
-            {/* iframe貼り付けで実装 */}
-            <div className="shadow-md rounded-xl overflow-hidden">
-              <iframe className="shadow-md rounded-xl" data-testid="embed-iframe" style={{ borderRadius: '12px', border: 'none' }} 
-                src="https://open.spotify.com/embed/playlist/635C2n92A07J8urkBP5mqH?utm_source=generator&theme=0&si=7b697c90d8de4a24"
-                width="100%" height="720" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy">
-              </iframe>
-            </div>
-          </section>
-
-          <section className="mt-12 max-w-3xl mx-auto bg-indigo-50 p-8 rounded-2xl border border-indigo-100">
-            <h2 className="text-center text-xl font-bold mb-4 border-b-2 border-indigo-200 pb-2">
-                  猫画像切らしてたので助かる
-            </h2>
-            <div className="space-y-4">
-              <section className="mt-12" id="cat_section">
-                {/* catImageUrlがある時「だけ」、Imageコンポーネントを表示 */}
-                {/* GitHub側と同じく三項演算子（条件付きレンダリング）*/}
-                {catImageUrl ? (
-                  <div className="mt-6 flex justify-center">
-                    <div 
-                      className="w-auto h-auto rounded-xl overflow-hidden" 
-                      style={{ width: '100%', height: '500px', position: 'relative', }}
-                    >
-                      <Image
-                      src={catImageUrl}
-                      alt="かわいいねこの画像"
-                      fill={true}
-                      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                      className="w-auto h-auto"
-                      onLoad={whenImageLoaded}
-                      loading="eager"
-                      fetchPriority="high"
-                      />
-                    </div>
-                  </div>
-                  ) : (
-                  <div className="flex justify-center">
-                    <p className="text-center py-8 text-gray-500 text-sm animate-pulse">
-                      ねこを読み込み中。。。ちょっとだけまってね
-                    </p>
-                  </div>    
-                  )    
-                }
-              </section>
-              <button 
-                onClick={SpawnBtnClick}
-                className="w-full bg-indigo-600 text-white py-2 rounded-md font-bold hover:bg-indigo-700 transition-all shadow-lg active:scale-95">
-                新しいねこを呼ぶ
-              </button>
-            </div>
-          </section>
-        </div>
-      </main>
-      <Footer />
-    </div>
-  );
+    <div>
+      <MainContent
+        InitGitItems={gitData}
+        InitCatItems={catData}
+      />
+    </div>  
+  )
 }
